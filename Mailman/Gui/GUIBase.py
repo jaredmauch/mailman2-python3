7c5c442f064452720038172287ac80dc6d877ad4
Jared Mauch <jared@puck.nether.net>
2025-04-26 20:31:15 -0400

    update

--- Mailman/Gui/GUIBase.py
+++ Mailman/Gui/GUIBase.py
@@ -45,6 +45,11 @@ class GUIBase:
             return int(val)
         # String and Text widgets both just return their values verbatim
         if wtype in (mm_cfg.String, mm_cfg.Text):
+            if isinstance(val, bytes):
+                try:
+                    return val.decode('utf-8')
+                except UnicodeDecodeError:
+                    return val.decode('latin1')
             return val
         # This widget contains a single email address
         if wtype == mm_cfg.Email:
@@ -52,6 +57,11 @@ class GUIBase:
             # be cleared.  This is currently the only mm_cfg.Email type widget
             # in the interface, so watch out if we ever add any new ones.
             if val:
+                if isinstance(val, bytes):
+                    try:
+                        val = val.decode('utf-8')
+                    except UnicodeDecodeError:
+                        val = val.decode('latin1')
                 # Let MMBadEmailError and MMHostileAddress propagate
                 Utils.ValidateEmail(val)
             return val
@@ -63,6 +73,11 @@ class GUIBase:
             # config_list input.  Sigh.
             if isinstance(val, ListType):
                 return val
+            if isinstance(val, bytes):
+                try:
+                    val = val.decode('utf-8')
+                except UnicodeDecodeError:
+                    val = val.decode('latin1')
             addrs = []
             bad_addrs = []
             for addr in [s.strip() for s in val.split(NL)]:
@@ -138,8 +153,14 @@ class GUIBase:
 
     def _setValue(self, mlist, property, val, doc):
         # Set the value, or override to take special action on the property
-        if not property.startswith('_') and getattr(mlist, property) != val:
-            setattr(mlist, property, val)
+        if not property.startswith('_'):
+            if isinstance(val, bytes):
+                try:
+                    val = val.decode('utf-8')
+                except UnicodeDecodeError:
+                    val = val.decode('latin1')
+            if getattr(mlist, property) != val:
+                setattr(mlist, property, val)
 
     def _postValidate(self, mlist, doc):
         # Validate all the attributes for this category
@@ -187,6 +208,11 @@ class GUIBase:
     # Convenience method for handling $-string attributes
     def _convertString(self, mlist, property, alloweds, val, doc):
         # Is the list using $-strings?
+        if isinstance(val, bytes):
+            try:
+                val = val.decode('utf-8')
+            except UnicodeDecodeError:
+                val = val.decode('latin1')
         dollarp = getattr(mlist, 'use_dollar_strings', 0)
         if dollarp:
             ids = Utils.dollar_identifiers(val)
