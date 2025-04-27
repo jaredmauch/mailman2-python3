0fe4b14a795da61c15ea9e8b35adf23dc7f1be69
Jared Mauch <jared@puck.nether.net>
2025-04-26 21:06:29 -0400

    update

--- Mailman/Queue/OutgoingRunner.py
+++ Mailman/Queue/OutgoingRunner.py
@@ -22,6 +22,7 @@ import sys
 import copy
 import time
 import socket
+import traceback
 
 import email
 
@@ -43,33 +44,67 @@ class OutgoingRunner(Runner, BounceMixin):
     QDIR = mm_cfg.OUTQUEUE_DIR
 
     def __init__(self, slice=None, numslices=1):
-        Runner.__init__(self, slice, numslices)
-        BounceMixin.__init__(self)
-        # We look this function up only at startup time
-        modname = 'Mailman.Handlers.' + mm_cfg.DELIVERY_MODULE
-        mod = __import__(modname)
-        self._func = getattr(sys.modules[modname], 'process')
-        # This prevents smtp server connection problems from filling up the
-        # error log.  It gets reset if the message was successfully sent, and
-        # set if there was a socket.error.
-        self.__logged = False
-        self.__retryq = Switchboard(mm_cfg.RETRYQUEUE_DIR)
+        syslog('debug', 'OutgoingRunner: Starting initialization')
+        try:
+            Runner.__init__(self, slice, numslices)
+            syslog('debug', 'OutgoingRunner: Base Runner initialized')
+            
+            BounceMixin.__init__(self)
+            syslog('debug', 'OutgoingRunner: BounceMixin initialized')
+            
+            # We look this function up only at startup time
+            modname = 'Mailman.Handlers.' + mm_cfg.DELIVERY_MODULE
+            syslog('debug', 'OutgoingRunner: Attempting to import delivery module: %s', modname)
+            
+            try:
+                mod = __import__(modname)
+                syslog('debug', 'OutgoingRunner: Successfully imported delivery module')
+            except ImportError as e:
+                syslog('error', 'OutgoingRunner: Failed to import delivery module %s: %s', modname, str(e))
+                syslog('error', 'OutgoingRunner: Traceback: %s', traceback.format_exc())
+                raise
+                
+            try:
+                self._func = getattr(sys.modules[modname], 'process')
+                syslog('debug', 'OutgoingRunner: Successfully got process function from module')
+            except AttributeError as e:
+                syslog('error', 'OutgoingRunner: Failed to get process function from module %s: %s', modname, str(e))
+                syslog('error', 'OutgoingRunner: Traceback: %s', traceback.format_exc())
+                raise
+                
+            # This prevents smtp server connection problems from filling up the
+            # error log.  It gets reset if the message was successfully sent, and
+            # set if there was a socket.error.
+            self.__logged = False
+            syslog('debug', 'OutgoingRunner: Initializing retry queue')
+            self.__retryq = Switchboard(mm_cfg.RETRYQUEUE_DIR)
+            syslog('debug', 'OutgoingRunner: Initialization complete')
+        except Exception as e:
+            syslog('error', 'OutgoingRunner: Initialization failed: %s', str(e))
+            syslog('error', 'OutgoingRunner: Traceback: %s', traceback.format_exc())
+            raise
 
     def _dispose(self, mlist, msg, msgdata):
+        syslog('debug', 'OutgoingRunner: Processing message for list %s', mlist.internal_name())
         # See if we should retry delivery of this message again.
         deliver_after = msgdata.get('deliver_after', 0)
         if time.time() < deliver_after:
+            syslog('debug', 'OutgoingRunner: Message not ready for delivery yet, waiting until %s', 
+                   time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(deliver_after)))
             return True
         # Make sure we have the most up-to-date state
+        syslog('debug', 'OutgoingRunner: Loading list state')
         mlist.Load()
         try:
             pid = os.getpid()
+            syslog('debug', 'OutgoingRunner: Attempting to deliver message')
             self._func(mlist, msg, msgdata)
             # Failsafe -- a child may have leaked through.
             if pid != os.getpid():
-                syslog('error', 'child process leaked thru: %s', modname)
+                syslog('error', 'OutgoingRunner: child process leaked thru: %s', modname)
                 os._exit(1)
             self.__logged = False
+            syslog('debug', 'OutgoingRunner: Message delivered successfully')
         except socket.error:
             # There was a problem connecting to the SMTP server.  Log this
             # once, but crank up our sleep time so we don't fill the error
@@ -79,32 +114,30 @@ class OutgoingRunner(Runner, BounceMixin):
                 port = 'smtp'
             # Log this just once.
             if not self.__logged:
-                syslog('error', 'Cannot connect to SMTP server %s on port %s',
+                syslog('error', 'OutgoingRunner: Cannot connect to SMTP server %s on port %s',
                        mm_cfg.SMTPHOST, port)
                 self.__logged = True
             self._snooze(0)
             return True
         except Errors.SomeRecipientsFailed as e:
+            syslog('debug', 'OutgoingRunner: Some recipients failed: %s', str(e))
             # Handle local rejects of probe messages differently.
             if msgdata.get('probe_token') and e.permfailures:
+                syslog('debug', 'OutgoingRunner: Handling probe bounce')
                 self._probe_bounce(mlist, msgdata['probe_token'])
             else:
                 # Delivery failed at SMTP time for some or all of the
                 # recipients.  Permanent failures are registered as bounces,
                 # but temporary failures are retried for later.
-                #
-                # BAW: msg is going to be the original message that failed
-                # delivery, not a bounce message.  This may be confusing if
-                # this is what's sent to the user in the probe message.  Maybe
-                # we should craft a bounce-like message containing information
-                # about the permanent SMTP failure?
                 if e.permfailures:
+                    syslog('debug', 'OutgoingRunner: Queueing permanent failures as bounces')
                     self._queue_bounces(mlist.internal_name(), e.permfailures,
                                         msg)
                 # Move temporary failures to the qfiles/retry queue which will
                 # occasionally move them back here for another shot at
                 # delivery.
                 if e.tempfailures:
+                    syslog('debug', 'OutgoingRunner: Queueing temporary failures for retry')
                     now = time.time()
                     recips = e.tempfailures
                     last_recip_count = msgdata.get('last_recip_count', 0)
@@ -114,6 +147,7 @@ class OutgoingRunner(Runner, BounceMixin):
                         # delivery any longer.  BAW: is this the best
                         # disposition?
                         if now > deliver_until:
+                            syslog('debug', 'OutgoingRunner: No progress made, giving up on message')
                             return False
                     else:
                         # Keep trying to delivery this message for a while
@@ -125,11 +159,17 @@ class OutgoingRunner(Runner, BounceMixin):
                     msgdata['deliver_until'] = deliver_until
                     msgdata['recips'] = recips
                     self.__retryq.enqueue(msg, msgdata)
+        except Exception as e:
+            syslog('error', 'OutgoingRunner: Unexpected error during message processing: %s', str(e))
+            syslog('error', 'OutgoingRunner: Traceback: %s', traceback.format_exc())
+            raise
         # We've successfully completed handling of this message
         return False
 
     _doperiodic = BounceMixin._doperiodic
 
     def _cleanup(self):
+        syslog('debug', 'OutgoingRunner: Starting cleanup')
         BounceMixin._cleanup(self)
         Runner._cleanup(self)
+        syslog('debug', 'OutgoingRunner: Cleanup complete')
