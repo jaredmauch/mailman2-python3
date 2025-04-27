f28a5dde551f81b5fc480e631a80cbdddfded71c
Jared Mauch <jared@puck.nether.net>
2025-04-27 10:02:07 -0400

    update error handling

--- Mailman/Queue/__init__.py
+++ Mailman/Queue/__init__.py
@@ -13,3 +13,50 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software 
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+"""Mailman Queue package initialization.
+
+This package contains the queue runners that process various types of messages
+in the Mailman system.
+"""
+
+import os
+import sys
+
+# Add the parent directory to the Python path if it's not already there
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+
+# Import the base Runner class first
+from Mailman.Queue.Runner import Runner
+
+# Then import the Switchboard
+from Mailman.Queue.Switchboard import Switchboard
+
+# Import other runners that don't have dependencies
+from Mailman.Queue.OutgoingRunner import OutgoingRunner
+from Mailman.Queue.NewsRunner import NewsRunner
+from Mailman.Queue.BounceRunner import BounceRunner
+from Mailman.Queue.MaildirRunner import MaildirRunner
+from Mailman.Queue.RetryRunner import RetryRunner
+from Mailman.Queue.CommandRunner import CommandRunner
+from Mailman.Queue.ArchRunner import ArchRunner
+
+# Import IncomingRunner and VirginRunner last to avoid circular imports
+from Mailman.Queue.IncomingRunner import IncomingRunner
+from Mailman.Queue.VirginRunner import VirginRunner
+
+__all__ = [
+    'Runner',
+    'Switchboard',
+    'OutgoingRunner',
+    'NewsRunner',
+    'BounceRunner',
+    'MaildirRunner',
+    'RetryRunner',
+    'CommandRunner',
+    'ArchRunner',
+    'IncomingRunner',
+    'VirginRunner',
+]
