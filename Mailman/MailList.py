b653968bb58d2989507b55c5e9cf776ed96c5db2
Jared Mauch <jared@puck.nether.net>
2025-05-14 21:49:51 -0400

    update

--- Mailman/MailList.py
+++ Mailman/MailList.py
@@ -96,7 +96,7 @@ class MailList(HTMLFormatter, Deliverer, ListAdmin,
             # pipe to the wrapper in an MTA alias or other delivery process
             # contains shell special characters so allow only defined characters
             # (default = '[-+_.=a-z0-9]').
-            if len(re.sub(mm_cfg.ACCEPTABLE_LISTNAME_CHARACTERS, '', name)) > 0:
+            if len(re.sub(r'[%s]' % mm_cfg.ACCEPTABLE_LISTNAME_CHARACTERS, '', name)) > 0:
                 raise Errors.BadListNameError(name)
             # Validate what will be the list's posting address
             postingaddr = '%s@%s' % (name, mm_cfg.DEFAULT_EMAIL_HOST)
